ef4ef731a33a856ee3e1a5b180c2bd4d85dd6135
Amina Said Ibrahim <amina7585@gmail.com>
2025-01-27 22:33:36 +0000

    did some styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 export default function App() {
   return (
     <>
-      <h1>Cookie Clicker Game</h1>
+      <div className="App">
+        <h1>Cookie Clicker Game</h1>
+      </div>
     </>
   );
 }
